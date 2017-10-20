3650cc44ab61335ada6b0cfe0a76de26ba7f595f
wski <contact.mattdylan@gmail.com>
2017-10-19 21:01:50 -0600

    gas prices

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,20 @@
-var Item = artifacts.require("./Item.sol");
-var Meta = artifacts.require("./Meta.sol");
-var Trade = artifacts.require("./Trade.sol");
-var Token = artifacts.require("./ERC20/Token.sol");
-var StandardToken = artifacts.require("./ERC20/StandardToken.sol");
-var AtomicInventory = artifacts.require("./AtomicInventory.sol");
+const Item = artifacts.require("./Item.sol")
+const Meta = artifacts.require("./Meta.sol")
+const Trade = artifacts.require("./Trade.sol")
+const Token = artifacts.require("./ERC20/Token.sol")
+const StandardToken = artifacts.require("./ERC20/StandardToken.sol")
+const AtomicInventory = artifacts.require("./AtomicInventory.sol")
+
+const gasPrice = 5e10
 
 module.exports = function(deployer) {
-  deployer.deploy(Token, { gas: 4700000 });
-  deployer.link(Token, StandardToken, { gas: 4700000 });
-  deployer.deploy(StandardToken, { gas: 4700000 });  
-  deployer.link(StandardToken, Item, { gas: 4700000 });
-  deployer.deploy(Item, { gas: 4700000 });
-  deployer.deploy(Meta, { gas: 4700000 });
-  deployer.deploy(Trade, { gas: 4700000 });
-  deployer.link(AtomicInventory, Meta, Trade, { gas: 4700000 });
-  deployer.deploy(AtomicInventory, { gas: 4700000 });
-};
+  deployer.deploy(Token, { gas: gasPrice })
+  deployer.link(Token, StandardToken)
+  deployer.deploy(StandardToken, { gas: gasPrice }); 
+  deployer.link(StandardToken, Item)
+  deployer.deploy(Item, { gas: gasPrice })
+  deployer.deploy(Meta, { gas: gasPrice })
+  deployer.deploy(Trade, { gas: gasPrice })
+  deployer.link(AtomicInventory, Meta, Trade)
+  deployer.deploy(AtomicInventory, { gas: gasPrice })
+}
